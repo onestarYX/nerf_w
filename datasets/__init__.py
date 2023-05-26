8d99f8628561c0ef28a612f2c1083cae44f09901
Yixing Wang <yxonestarw@gmail.com>
2023-05-26 16:05:16 -0700

    added code for taking behave dataset as the input

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,7 +1,9 @@
 from .blender import BlenderDataset
 from .llff import LLFFDataset
 from .phototourism import PhototourismDataset
+from .behave import BehaveDataset
 
 dataset_dict = {'blender': BlenderDataset,
                 'llff': LLFFDataset,
-                'phototourism': PhototourismDataset}
+                'phototourism': PhototourismDataset,
+                'behave': BehaveDataset}
